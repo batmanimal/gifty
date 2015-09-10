06b258ef31887283f8158df23167cbfb8f1ae722
batmanimal <thejenniekim@gmail.com>
2015-09-09 17:54:59 -0700

    Create daysFromToday property for friends' bdays

--- src/app/Reducers/userReducer.js
+++ src/app/Reducers/userReducer.js
@@ -19,10 +19,46 @@ export default function userReducer(state=initialState, action) {
 
     case FETCH_FRIENDS:
 
-      var friends = [];
-      action.friends.forEach(function(friend) {
-        friends.push(friend);
-      });
+      var getDaysFromToday = function(birthday) {
+        if(!birthday) {
+          return null;
+        }
+        var today = new Date();
+        var currentYear = today.getFullYear();
+        var friendBdayArray = birthday.split('/');
+        var friendBdayMonth = friendBdayArray[0];
+        var friendBdayDD = friendBdayArray[1];
+        // replace year with current year 
+        var friendBday = new Date(currentYear, friendBdayMonth, friendBdayDD);
+        // calculate days from now to the bday; getTime() returns time in ms 
+        var elapsed = friendBday.getTime() - today.getTime();
+        // 86,400,000 ms per day
+        var daysFromToday = Math.floor(elapsed / 86400000); 
+
+        // add 365 days to get upcoming day 
+        if(daysFromToday < 0) {
+          daysFromToday = daysFromToday + 365;
+        }
+        // console.log("daysFromToday for " + daysFromToday + " for bday " + birthday);
+        return daysFromToday;    
+      };
+
+    var friends = [];
+    action.friends.forEach(function(friend) {
+      friend.daysFromToday = getDaysFromToday(friend.birthday);
+      friends.push(friend);
+    });
+
+    friends.sort(function(a, b){
+      if (a.daysFromToday === null) {
+        return 1;
+      }
+      if (b.daysFromToday === null) {
+        return -1;
+      }
+      return a.daysFromToday - b.daysFromToday;
+    });
+
       return Object.assign({}, state, { friends: friends });
 
     case GET_USER:
